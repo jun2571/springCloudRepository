859bd370a734762d14cc8123e2657df79cbbb2df
lizhu <1020913920@qq.com>
2019-10-25 15:05:36 +0800

    git提交测试

---
+++ src/main/java/com/yun/TestGit.java
@@ -0,0 +1,9 @@
+package com.yun;
+
+import java.util.Arrays;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
