43af239380b2753aabe61ebea1c003022ec2da15
lizhu <1020913920@qq.com>
2019-10-25 15:31:17 +0800

    git提交测试

--- src/main/java/com/yun/TestGit2.java
+++ src/main/java/com/yun/TestGit2.java
@@ -4,6 +4,17 @@ import java.util.Arrays;
 
 public class TestGit2 {
     public static void main(String[] args) {
+
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("args = " + Arrays.deepToString(args));
         System.out.println("args = " + Arrays.deepToString(args));
     }
 }
